4520a8969ea3ff374eb2ad9dacb343235d5d2ffb
Muhammad Muad Thaha <MUADTHAHA@GMAIL.COM>
2025-01-31 02:05:40 +0800

    Revert "Upload nest Project"
    This reverts commit 31c0c8d21a706173d625c9337746292a7d2eb26f.

--- src/app.service.ts
+++
@@ -1,8 +0,0 @@
-import { Injectable } from "@nestjs/common";
-
-@Injectable()
-export class AppService {
-    getHello(): string {
-        return "Hello World!";
-    }
-}   // This is the service that the controller will use to get the data that it will return to the client.
